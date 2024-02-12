48f042009e1063a212ce839d2cab83e0b2cdcd5a
Amit <amit101giri@hotmail.com>
2024-02-12 10:49:52 +0545

    manage product and manage order view

--- ecom/vendor/urls.py
+++ ecom/vendor/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "vendor"
 urlpatterns = [
     path("seller/",views.DashboardView.as_view(),name="dashboard"),
+    path("manageproduct/",views.ManageProductView.as_view(),name="manageproduct"),
+    path("manageorder/",views.ManageOrderView.as_view(),name="manageorder"),
 ]
--- ecom/vendor/views.py
+++ ecom/vendor/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.views import generic
-
+from store.models import Product,Order
 
 class DashboardView(generic.TemplateView):
     template_name = "vendor/dashboard.html"
+
+class ManageProductView(generic.ListView):
+    template_name = "vendor/manageproduct.html"
+    model = Product
+    context_object_name = "product_list"
+
+    def get_queryset(self):
+        return Product.objects.filter(owner=self.request.user)
+
+class ManageOrderView(generic.ListView):
+    template_name = "vendor/manageorder.html"
+    model = Order
+    context_object_name = "order_list"
+
+    def get_queryset(self):
+        return Order.objects.filter(product__owner=self.request.user)
+    
